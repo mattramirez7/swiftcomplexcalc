bfb834cf11b079f3b5f2e001a67ebfe6704df764
mattramirez7 <77175367+mattramirez7@users.noreply.github.com>
2022-04-11 18:09:02 -0700

    basic add, subtract, multiply, divide works

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -4,6 +4,26 @@ print("Welcome back to the UW Calculator")
 // below both compile and return "true"
 class Calculator {
     
+    func add(lhs : Int, rhs : Int) -> Int {
+        return lhs + rhs
+    }
+    
+    
+    func subtract(lhs : Int, rhs : Int) -> Int {
+        return lhs - rhs
+    }
+    
+    
+    func multiply(lhs : Int, rhs : Int) -> Int {
+        return lhs * rhs
+    }
+    
+    
+    func divide(lhs : Int, rhs : Int) -> Int {
+        return lhs / rhs
+    }
+    
+    
 }
 
 let calc = Calculator()  // Don't change this declaration name; it's used in all the tests below
@@ -16,7 +36,7 @@ calc.add(lhs: 2, rhs: 2) == 4
 calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
-
+/*
 calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
@@ -50,3 +70,4 @@ let pd1 = ["x": 5, "y": 5]
 let pd2 = ["x": -4, "y": 4]
 calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9]
 calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1]
+*/
